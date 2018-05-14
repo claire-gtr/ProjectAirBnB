7506d2a96102f3ae0a94f88d437a4cad35de2dc3
Maximilien Renaut <max.renaut@gmail.com>
2018-05-14 19:02:26 +0200

    navbar seed devise car_view index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
 Car.destroy_all
+kevin = User.create!(email: "kevin@gmail.com", password: "kekedu38")
+
+
 puts 'Creating cars...'
 cars_attributes = [
   {
+    user: kevin,
     name: "La ratisseuse",
     model: "Citroen BX",
     description: "Jantes targa racing 15 pouces, rétros m3, hp oxygen 3 voies 170w source sony s2200 cdr ampli boa 800w + caisson accoustique 1000w jbl"
   },
   {
+    user: kevin,
     name: "Le lézard",
     model: "Peugeot 206",
     description: "Néon cup, jantes atomik 17 pouces, son surround, portes papillon, prises d'air racing"
   },
   {
+    user: kevin,
     name: "La fusée de la galaxie",
     model: "Renault r21",
     description: "Ailerons de toit, paupières de phare et de pare brise, échappement quadri-sorties"
@@ -27,4 +33,3 @@ cars_attributes = [
 ]
 Car.create!(cars_attributes)
 puts 'Finished!'
-
