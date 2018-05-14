3411cf3be2d495f01a5f45abc5c14c4cab452faa
Claire Gautier <claire.gautier@edhec.com>
2018-05-14 17:47:44 +0200

    change migration fix db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_14_152742) do
+ActiveRecord::Schema.define(version: 2018_05_14_152743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "start_time"
+    t.string "end_time"
+    t.bigint "user_id"
+    t.bigint "car_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "name"
@@ -45,5 +56,7 @@ ActiveRecord::Schema.define(version: 2018_05_14_152742) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cars", "users"
 end
