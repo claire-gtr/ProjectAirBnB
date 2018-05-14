2b2fc1259ea7afa12677d2b4567ebf285dbcf780
Claire Gautier <claire.gautier@edhec.com>
2018-05-14 15:10:49 +0200

    add new create edit update destro to Car controller and edit view

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,42 @@
 class CarsController < ApplicationController
+
+
+
+
+  def new
+    @cars = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to car_path(@car)
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :description, :model, :address)
+  end
+
 end
